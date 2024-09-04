54da99a438cafb17dbbb34f892d42797e69badac
MinhKQ <minhkieu702@gmail.com>
2024-09-05 04:34:08 +0700

    m

---
+++ HomeeBackEnd/Homee.DataLayer/RequestModels/PlaceRequest.cs
@@ -0,0 +1,38 @@
+﻿using Homee.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Homee.DataLayer.RequestModels
+{
+    public class PlaceRequest
+    {
+        public string Province { get; set; } = null!;
+
+        public string Distinct { get; set; } = null!;
+
+        public string Ward { get; set; } = null!;
+
+        public string Street { get; set; } = null!;
+
+        public string Number { get; set; } = null!;
+
+        public double Area { get; set; }
+
+        public int Direction { get; set; }
+
+        public int NumberOfToilet { get; set; }
+
+        public int NumberOfBedroom { get; set; }
+
+        public double Rent { get; set; }
+        public List<int> Categories { get; set; } = [];
+        [JsonIgnore]
+        public int OwnerId { get; set; } = 1;
+        [JsonIgnore]
+        public bool? IsBlock { get; set; } = false;
+    }
+}
---
+++ HomeeBackEnd/Homee.DataLayer/ResponseModels/PlaceResponse.cs
@@ -0,0 +1,48 @@
+﻿using Homee.DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homee.DataLayer.ResponseModels
+{
+    public class PlaceResponse
+    {
+        public int PlaceId { get; set; }
+
+        public string Province { get; set; } = null!;
+
+        public string Distinct { get; set; } = null!;
+
+        public string Ward { get; set; } = null!;
+
+        public string Street { get; set; } = null!;
+
+        public string Number { get; set; } = null!;
+
+        public double Area { get; set; }
+
+        public string Direction { get; set; }
+
+        public int NumberOfToilet { get; set; }
+
+        public int NumberOfBedroom { get; set; }
+
+        public double Rent { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public bool? IsBlock { get; set; }
+
+        public virtual ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+
+        public virtual ICollection<Interior> Interiors { get; set; } = new List<Interior>();
+
+        public virtual Account Owner { get; set; } = null!;
+
+        public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+
+        public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
