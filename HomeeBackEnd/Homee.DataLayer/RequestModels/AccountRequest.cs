8d255b12e60d69d30c25a52f85993e0389f810d4
MinhKQ <minhkieu702@gmail.com>
2024-09-05 04:33:57 +0700

    m

---
+++ HomeeBackEnd/Homee.DataLayer/RequestModels/AccountRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homee.DataLayer.RequestModels
+{
+    public class AccountRequest
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
+        public string CitizenId { get; set; } = null!;
+        
+        public int Role { get; set; }
+
+        public DateOnly? BirthDay { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
